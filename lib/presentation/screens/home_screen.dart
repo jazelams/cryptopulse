fb9a7100e83c423b4c7a95df380da96ae50fc9b2
jazelams <jazelams@gmail.com>
2026-02-21 16:40:18 -0600

    Funcion y boton de cambio de tema,funcionando.

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,12 +1,15 @@
+import 'package:cryptopulse/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import '../widgets/crypto_card.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(themeProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -16,6 +19,14 @@ class HomeScreen extends StatelessWidget {
         centerTitle: true,
         actions: [
           IconButton(
+            icon: Icon(
+              themeMode == ThemeMode.dark ? Icons.light_mode : Icons.dark_mode,
+            ),
+            onPressed: () {
+              ref.read(themeProvider.notifier).toggleTheme();
+            },
+          ),
+          IconButton(
             icon: const Icon(Icons.settings),
             onPressed: () =>
                 context.push('/settings'), // Botón de configuración
@@ -24,62 +35,58 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column (
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children:[
+          children: [
+            const SizedBox(height: 16),
 
-          const SizedBox(height: 16),
-
-          const Text(
-            "Mercado",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+            const Text(
+              "Mercado",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-          ),
 
-        const SizedBox(height: 4),
-        Text(
-          "Precios actuales de Criptomoneda",
-          style: TextStyle(color: Colors.grey[400]),
+            const SizedBox(height: 4),
+            Text(
+              "Precios actuales de Criptomoneda",
+              style: TextStyle(color: Colors.grey[400]),
+            ),
+
+            const SizedBox(height: 10),
+
+            Divider(color: Colors.grey[800]),
+
+            const SizedBox(height: 10),
+
+            Expanded(
+              child: ListView(
+                children: const [
+                  CryptoCard(
+                    name: 'Bitcoin',
+                    symbol: 'BTC',
+                    price: '\$64,230.00',
+                    change: '+2.5%',
+                    isPositive: true,
+                  ),
+                  CryptoCard(
+                    name: 'Ethereum',
+                    symbol: 'ETH',
+                    price: '\$3,450.50',
+                    change: '-1.2%',
+                    isPositive: false,
+                  ),
+                  CryptoCard(
+                    name: 'Solana',
+                    symbol: 'SOL',
+                    price: '\$145.20',
+                    change: '+5.7%',
+                    isPositive: true,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-
-        const SizedBox(height: 10),
-
-        Divider(color: Colors.grey[800]),
-
-        const SizedBox(height: 10),
-
-        Expanded(
-          child: ListView(
-            children: const [
-              CryptoCard(
-                name:'Bitcoin',
-                symbol:'BTC',
-                price:'\$64,230.00',
-                change:'+2.5%',
-                isPositive: true,
-              ),
-              CryptoCard(
-                name:'Ethereum',
-                symbol:'ETH',
-                price:'\$3,450.50',
-                change:'-1.2%',
-                isPositive:false,
-              ),
-              CryptoCard(
-                name:'Solana',
-                symbol:'SOL',
-                price:'\$145.20',
-                change:'+5.7%',
-                isPositive: true,
-              ),
-            ],
-          ),
-        ),
-      ],
-    ),
-  ),
+      ),
     );
   }
 }
